e1ce9bb50cbc2b9551ff0d0c9b2950b52dc636d5
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2021-03-01 20:41:22 -0800

    lets get normal
    added a ton of normal mapps and also the door opens

---
+++ Assets/constrain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class constrain : MonoBehaviour
+{
+
+
+//public Transform FrontOfCorridor;
+//public Transform BackOfCorridor;
+public float MoveSpeed;
+public Vector3 teleport;
+
+void Start() {
+  //transform.position = BackOfCorridor.position;
+}
+ 
+void Update() {
+  if (Input.GetKey(KeyCode.B)) {
+    transform.position = Vector3.MoveTowards(transform.position, teleport, MoveSpeed * Time.deltaTime);
+  }
+  //if (Input.GetKey(KeyCode.S)) {
+  //  transform.position = Vector3.MoveTowards(transform.position, FrontOfCorridor.position, MoveSpeed * Time.deltaTime);
+  //}
+}
+
+}
